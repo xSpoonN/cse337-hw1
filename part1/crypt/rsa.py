90ca0188375cb1300c7abff70c23b57d1334585f
Kevin Tao <kevin.tao@stonybrook.edu>
2022-09-15 22:32:02 -0400

    Part 1 done

--- part1/crypt/rsa.py
+++ part1/crypt/rsa.py
@@ -1,15 +1,38 @@
 import random as rd
-# import the mymath module here.
+import mymath
+from mymath.mymath import isPrime, gcd, lcm, pubkExp, prikExp, hash
 
 class Rsa:
     # initialize to set p, q, and n.
-    def __init__():
-        pass
-
+    
+    def __init__(self, p):
+        prime = []
+        for item in range(p, p+1000):
+            if isPrime(item):
+                prime.append(item)
+        self.p = rd.choice(prime)
+        self.q = rd.choice(prime)
+        while self.p == self.q:
+            self.q = rd.choice(prime)
+        self.n = self.p*self.q
+        
     # generates a cipher string for a message m
-    def encrypt():
-        pass
+    def encrypt(self, m):
+        k = lcm(self.p-1, self.q-1)
+        e = pubkExp(k)
+        # c = (m^e) % n
+        c = 1; iter = 0
+        while (iter < e):
+            iter += 1
+            c = (m * c) % self.n
+        return c, e
 
     # decrypts a cipher string to get back original message
     def decrypt(self, c, e):
-        pass
+        k = lcm(self.p-1, self.q-1)
+        d = prikExp(e, int(k))
+        m = 1; iter = 0
+        while (iter < d):
+            iter += 1
+            m = (c * m) % self.n
+        return m
--- part1/mymath/mymath.py
+++ part1/mymath/mymath.py
@@ -1,22 +1,38 @@
 import random as rd
 
 def isPrime(n):
-    pass
+    i = 2
+    while i * i <= n:
+        if n % i == 0: return False
+        i += 1
+    return True
 
 def gcd(a,b):
-    pass
+    if a == 0: return b
+    return gcd(b % a, a)
 
 def lcm(a,b):
-    pass
+    return (a*b)/gcd(a,b)
 
 # Generates public key exponent
 def pubkExp(k):
-    pass
+    e = 2
+    while gcd(e, k) != 1:
+         e += 1
+    return e
 
 # Generate private key exponent
 def prikExp(x, y):
-    pass
+    if (x > y):
+        t = x; x = y; y = t
+    for item in range(1, y):
+        if (((item % y) * (x % y)) % y) == 1:
+            return item
+    return -1
 
 # Returns the hash of a string message. Sum of its ascii characters.
 def hash(s):
-    pass
+    sum = 0
+    for c in s:
+        sum += ord(c)
+    return sum
