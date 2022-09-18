3e5faf8643df6567990f3fee2633e3881d142e4d
Kevin Tao <kevin.tao@stonybrook.edu>
2022-09-18 17:27:36 -0400

    Surely done this time

--- part2/utils/commands.py
+++ part2/utils/commands.py
@@ -1,5 +1,5 @@
 # import db/manager here.
-from part2.db.manager import add_task, change_priority, complete_task, get_all_tasks, is_tasks_file_exists, remove_task, search, sort, update_desc
+from db.manager import add_task, change_priority, complete_task, get_all_tasks, is_tasks_file_exists, remove_task, search, sort, update_desc
 
 def showhelp():
     print('usage: python main.py <options>')
@@ -72,6 +72,3 @@ def search_cmd(id, desc, priority):
 def sort_cmd(order):
     if (order == "-d"): return sort(2)
     else: return sort(1)
-
-def gettasks():
-    return get_all_tasks()
